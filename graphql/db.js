0202a06939285b484cb3391aee4a4986055a2c37
JunseopPark <metternichch@gmail.com>
2019-09-15 22:57:33 +0900

    Delete Mutation
    Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,36 +1,36 @@
 let movies = [
   {
-    id: "0",
+    id: 0,
     name: "Nicolas",
     score: 18
   },
   {
-    id: "1",
+    id: 1,
     name: "Jisu",
     score: 18
   },
   {
-    id: "2",
+    id: 2,
     name: "Japan Guy",
     score: 18
   },
   {
-    id: "3",
+    id: 3,
     name: "Daal",
     score: 18
   },
   {
-    id: "4",
+    id: 4,
     name: "JD",
     score: 18
   },
   {
-    id: "5",
+    id: 5,
     name: "moondaddi",
     score: 18
   },
   {
-    id: "6",
+    id: 6,
     name: "flynn",
     score: 18
   }
@@ -39,12 +39,12 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
+  const filteredPeople = people.filter(person => person.id === id);
   return filteredPeople[0];
 };
 
 export const deleteMovie = id => {
-  const cleanedMovies = movies.fileter(movie => movie.id !== String(id));
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
 
   if (movies.length > cleanedMovies.length) {
     movies = cleanedMovies;
